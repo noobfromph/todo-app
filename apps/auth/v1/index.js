92880d0496606b926932e9765da8e61e4b3e92c7
noobfromph <elungay@gmail.com>
2023-06-05 16:14:32 +0800

    fix: docs

--- apps/auth/v1/index.js
+++ apps/auth/v1/index.js
@@ -9,14 +9,14 @@ const authenticateUser = require('../../../middlewares/auth');
 
 const controller = require('./controller');
 
-// schema for adding or updating todo
+// schema for sign in
 const signInSchema = Joi.object({
     username: Joi.string().required(),
     password: Joi.string().required(),
     nonce: Joi.string().required()
 });
 
-// routes for todo
+// routes for auth
 function routes() {
     router.post('/nonces', controller.generateNonce());
     router.post('/signin', validator.body(signInSchema), controller.signIn());
